8c4592c1fff9bcc41c219f94c04eb3e9166aef53
AmyM6 <101350236+AmyM6@users.noreply.github.com>
2024-04-10 09:23:58 -0600

    Created index.html and script.js files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create a function
+function showPunchline() {
+    // access the DOM by .getElementById()
+    let punchlineElement = document.getElementById("punchline");
+    // Change the DOM by using innerHTML
+    punchlineElement.innerHTML = "You console it!";
+}
